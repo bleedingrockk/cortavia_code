51dd90cc3f2991bb096844bd0cc788241de446ce
sureshneostats <suresh.suthar@theneostats.com>
2025-07-18 11:43:57 +0530

    added cahtbot

--- chatbot/llm.py
+++ chatbot/llm.py
@@ -1,9 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+GEMINI_KEY = os.getenv("GEMINI_KEY")
 # Initialize model
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
-    api_key="AIzaSyDcy_g83otcsIagJAXH_VjSD1AEcYiKdGM",
+    api_key=GEMINI_KEY,
     temperature=0,
     max_tokens=None,
     timeout=None,
